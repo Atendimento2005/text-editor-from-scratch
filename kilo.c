f1be51344582db538d507b993d45240c7c673dde
Archisman Das <gogo.june2005@gmail.com>
2024-06-10 20:13:57 +0530

    draw tildae

--- kilo.c
+++ kilo.c
@@ -6,7 +6,8 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
-#include  <sys/ioctl.h>
+#include <sys/ioctl.h>
+#include <string.h>
 
 // defines
 
@@ -73,18 +74,17 @@ int getCursorPosition(int *rows, int *cols) {
     i++;
   }
   buf[i] = '\0';
-  
-  printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
-  
-  editorReadKey();
+ 
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
 
-  return -1;
+  return 0;
 }
 
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
-  if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
     if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
     return getCursorPosition(rows, cols);
   } else {
@@ -94,12 +94,25 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+// append buffer
+
+struct abuf {
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
 // output
 
 void editorDrawRows() {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    write(STDOUT_FILENO, "~", 1);
+
+    if (y < E.screenrows -1) {
+      write (STDOUT_FILENO, "\r\n", 2);
+    }
   }
 }
 
