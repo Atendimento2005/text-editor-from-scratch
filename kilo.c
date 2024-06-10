02943521c09db3200d5ca2bccb35dbd026e41529
Archisman Das <gogo.june2005@gmail.com>
2024-06-10 20:43:15 +0530

    append buffer implemented

--- kilo.c
+++ kilo.c
@@ -103,26 +103,54 @@ struct abuf {
 
 #define ABUF_INIT {NULL, 0}
 
+void abAppend(struct abuf *ab, const char *s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab){
+  free(ab->b);
+}
+
 // output
 
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    write(STDOUT_FILENO, "~", 1);
+    // write(STDOUT_FILENO, "~", 1);
+    abAppend(ab, "~", 1);
 
     if (y < E.screenrows -1) {
-      write (STDOUT_FILENO, "\r\n", 2);
+      // write (STDOUT_FILENO, "\r\n", 2);
+      abAppend(ab, "\r\n", 2);
     }
   }
 }
 
 void editorRefreshScreen(){
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  struct abuf ab = ABUF_INIT;
 
-  editorDrawRows();
+  printf("len is %d\r\n", ab.len);
+  abAppend(&ab, "\x1b[2J", 4);
+  printf("len is %d\r\n", ab.len);
+  abAppend(&ab, "\x1b[H", 3);
+  printf("len is %d\r\n", ab.len);
+  // write(STDOUT_FILENO, "\x1b[2J", 4);
+  // write(STDOUT_FILENO, "\x1b[H", 3);
 
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  editorDrawRows(&ab);
+  printf("len is %d\r\n", ab.len);
+
+  abAppend(&ab, "\x1b[H", 3);
+  printf("len is %d\r\n", ab.len);
+  // write(STDOUT_FILENO, "\x1b[H", 3);
+  
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
 }
 
 // input
